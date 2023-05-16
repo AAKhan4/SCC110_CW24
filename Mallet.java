71095287d2308aa904371694411ee7287fc4caf3
Aditya <a.a.khan@lancaster.ac.uk>
2023-05-16 15:23:08 +0100

    Modified mallet movement for better responsiveness. Changed pause() function to run the game at equivalent of 100 fps.

--- Mallet.java
+++ Mallet.java
@@ -22,26 +22,76 @@ public class Mallet extends Mover{
     public void moveVertical(GameArena arena, boolean up, boolean down, int leftEdge, int rightEdge) {
         boundaryHit(leftEdge, rightEdge);
         setYAcc(0);
-        if (up) setYAcc(-0.5);
-        else if (down) setYAcc(0.5);
+        if (up) {
+            if (getYVel()<=-8) {
+                setYAcc(0);
+                setYVel(-8);
+            }
+            if (getYVel()>-8) setYAcc(-0.1);
+            if (getYVel()>-7) setYAcc(-0.2);
+            if (getYVel()>-6.5) setYAcc(-0.3);
+            if (getYVel()>-6) setYAcc(-0.4);
+            if (getYVel()>-5) setYAcc(-0.7);
+            if (getYVel()>-3.5) setYAcc(-1.25);
+            if (getYVel()>-2.5) setYAcc(-1.75);
+            if (getYVel()>0) setYAcc(-2);
+        } else if (down) {
+            if (getYVel()>=8) {
+                setYAcc(0);
+                setYVel(8);
+            }
+            if (getYVel()<8) setYAcc(0.1);
+            if (getYVel()<7) setYAcc(0.2);
+            if (getYVel()<6.5) setYAcc(0.3);
+            if (getYVel()<6) setYAcc(0.4);
+            if (getYVel()<5) setYAcc(0.7);
+            if (getYVel()<3.5) setYAcc(1.25);
+            if (getYVel()<2.5) setYAcc(1.75);
+            if (getYVel()<0) setYAcc(2);
+        }
     }
 
     public void stopVertical(GameArena arena, boolean up, boolean down) {
         if (up || down) return;
-        if (getYVel()!=0) setYAcc(-(0.5*getYVel()));
+        if (getYVel()!=0) setYAcc(-(getYVel()/5));
         else setYAcc(0);
     }
 
     public void moveHorizontal(GameArena arena, boolean left, boolean right, int leftEdge, int rightEdge) {
         boundaryHit(leftEdge, rightEdge);
         setXAcc(0);
-        if (left) setXAcc(-0.5);
-        else if (right) setXAcc(0.5);
+        if (left) {
+            if (getXVel()<=-8) {
+                setXAcc(0);
+                setXVel(-8);
+            }
+            if (getXVel()>-8) setXAcc(-0.1);
+            if (getXVel()>-7) setXAcc(-0.2);
+            if (getXVel()>-6.5) setXAcc(-0.3);
+            if (getXVel()>-6) setXAcc(-0.4);
+            if (getXVel()>-5) setXAcc(-0.7);
+            if (getXVel()>-3.5) setXAcc(-1.25);
+            if (getXVel()>-2.5) setXAcc(-1.75);
+            if (getXVel()>0) setXAcc(-2);
+        } else if (right) {
+            if (getXVel()>=8) {
+                setXAcc(0);
+                setXVel(8);
+            }
+            if (getXVel()<8) setXAcc(0.1);
+            if (getXVel()<7) setXAcc(0.2);
+            if (getXVel()<6.5) setXAcc(0.3);
+            if (getXVel()<6) setXAcc(0.4);
+            if (getXVel()<5) setXAcc(0.7);
+            if (getXVel()<3.5) setXAcc(1.25);
+            if (getXVel()<2.5) setXAcc(1.75);
+            if (getXVel()<0) setXAcc(2);
+        }
     }
 
     public void stopHorizontal(GameArena arena, boolean left, boolean right) {
         if (left || right) return;
-        if (getXVel()!=0) setXAcc(-(0.5*getXVel()));
+        if (getXVel()!=0) setXAcc(-(getXVel()/5));
         else setXAcc(0);
     }
 
