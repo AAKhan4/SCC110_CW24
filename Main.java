b8d93cef3d89b34e4ea31a1dfe9c0836ae4049d9
Aditya <a.a.khan@lancaster.ac.uk>
2023-05-16 17:51:07 +0100

    added text to game arena

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         GameArena arena = new GameArena(1050, 600);
-        innitAirHockey(arena);
+        Text scoreDisplay1 = new Text("0", 50, 70, 350, "WHITE");
+        Text scoreDisplay2 = new Text("0", 50, 940, 350, "WHITE");
+        innitAirHockey(arena, scoreDisplay1, scoreDisplay2);
 
         Puck puck = new Puck(518, 338, arena);
         Mallet player1 = new Mallet(304, 338, arena);
@@ -15,7 +17,10 @@ public class Main {
         }
     }
 
-    private static void innitAirHockey(GameArena arena) {
+    private static void innitAirHockey(GameArena arena, Text scoreDisplay1, Text scoreDisplay2) {
+        arena.addText(new Text("WELCOME TO AIR HOCKEY!", 40, 250, 100, "WHITE"));
+        arena.addText(scoreDisplay1);
+        arena.addText(scoreDisplay2);
         arena.addRectangle(new Rectangle(180, 150, 675, 375, "BLUE", 0));
         arena.addRectangle(new Rectangle(190, 160, 655, 355, "WHITE", 0));
         arena.addRectangle(new Rectangle(190, 261, 7, 153, "GREY", 0));
